13a7d1000b6d57c7be85dcd4745b73a822da52a5
Moisés Madeira <msalcmd@gmail.com>
2023-06-05 10:13:02 -0300

    - added configuracao_inicial to main

--- GestaoGUI/main.py
+++ GestaoGUI/main.py
@@ -1,6 +1,7 @@
 from GestaoGUI.visao.Login import Login
 from GestaoGUI.visao.MainWindow import MainWindow
+from GestaoGUI.persistencia.banco import configuracao_inicial
 
 if __name__ == "__main__":
-
+    configuracao_inicial()
     MainWindow().mainloop()
