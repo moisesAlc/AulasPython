6bcd8a16b9937eca18b85ee22f23efed13b6b292
Moisés Madeira <msalcmd@gmail.com>
2023-06-05 10:13:42 -0300

    - added database files

--- GestaoGUI/persistencia/banco.py
+++ GestaoGUI/persistencia/banco.py
@@ -1,6 +1,13 @@
-import sqlite3
+from GestaoGUI.persistencia.automoveis_db import existem_dados
 
-con = sqlite3.connect("dados.db.db")
 
-cursor = con.cursor()
-
+def configuracao_inicial():
+    existem_dados()
+    '''
+self.vehicles_list = [
+            Automovel("Ford Mustang GT", "Ford", 2015, "branco", True),
+            Automovel("Hyundai Tiburon GT V6", "Hyundai", 2016, "cinza", False),
+            Automovel("Lamborghini Murciélago", "Lamborghini", 2017, "preto", True),
+            Automovel("Nissan 350Z", "Nissan", 2018, "verde perolado", False),
+            Automovel("Subaru Impreza WRX", "Subaru", 2019, "roxo", True),
+        ]'''
