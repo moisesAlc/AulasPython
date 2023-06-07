9f78cf839e0785c3814f51a29e31c6b09c8cef9d
Moisés Madeira <msalcmd@gmail.com>
2023-06-06 21:25:50 -0300

    reorganized code

--- GestaoGUI/persistencia/automoveis_db.py
+++ GestaoGUI/persistencia/automoveis_db.py
@@ -1,10 +1,10 @@
 import sqlite3
 
 con = sqlite3.connect("persistencia/dados.db")
-
 cursor = con.cursor()
 
 
 def existem_dados():
     result = cursor.execute("SELECT * FROM automoveis")
     print(result.fetchone())
+
