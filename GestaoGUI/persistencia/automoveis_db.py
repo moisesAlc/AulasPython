1f50ba78efe7fbb6d337e9aed136d7473bfa6a5a
Moisés Madeira <msalcmd@gmail.com>
2023-06-09 09:26:37 -0300

    corrected existem_dados & add retorna_automoveis

--- GestaoGUI/persistencia/automoveis_db.py
+++ GestaoGUI/persistencia/automoveis_db.py
@@ -1,3 +1,4 @@
+from GestaoGUI.modelos.Automovel import Automovel
 import sqlite3
 
 con = sqlite3.connect("persistencia/dados.db")
@@ -6,5 +7,20 @@ cursor = con.cursor()
 
 def existem_dados():
     result = cursor.execute("SELECT * FROM automoveis")
-    print(result.fetchone())
+    if result.fetchone():
+        print(result.fetchone())
+        return True
+    else:
+        return False
 
+
+def retorna_automoveis():
+    result = cursor.execute("SELECT * FROM automoveis").fetchall()
+    automoveis = []
+    for automovel in result:
+        '''print(automovel)
+        novo_automovel = Automovel()
+        novo_automovel.nome = automovel'''
+
+        automoveis.append(Automovel(automovel))
+    return automoveis
